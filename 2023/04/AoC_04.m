50f80c17ff69232cd84bfd4c666a82f596ccc393
Oliver Warlow <oliver@warlow.engineer>
2023-12-04 09:29:37 +0000

    2023 04

---
+++ 2023/04/AoC_04.m
@@ -0,0 +1,28 @@
+clear
+opts = detectImportOptions('04_input.txt');
+opts.SelectedVariableNames = [3:12];
+winners = readmatrix('04_input.txt',opts)
+opts.SelectedVariableNames = [14:38];
+ours = readmatrix('04_input.txt',opts)
+clear opts
+
+%%
+
+count = zeros([length(winners) 1]);
+for i = 1:length(ours)
+    count(i) = 2^(sum(ismember(winners(i,:),ours(i,:)))-1);
+end
+
+Ans1 = sum(count(count~=0.5))
+
+%%
+
+copies = ones([length(winners) 1]);
+for i = 1:length(ours)
+    for c = 1:copies(i)
+        wins = sum(ismember(winners(i,:),ours(i,:)));
+        copies(i+1:i+wins,:)=copies(i+1:i+wins,:)+1;
+    end
+end
+
+Ans2 = sum(copies)
