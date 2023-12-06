cb0a27e58faf17f7d8b73165d26d43c59a6adaa9
revilowaldow <oliver@warlow.engineer>
2023-12-06 19:29:32 +0000

    gcse maffs

--- 2023/06/AoC_06.m
+++ 2023/06/AoC_06.m
@@ -8,14 +8,27 @@ input = readmatrix("06_input.txt", opts)'
 time = input(:,1);
 distance = input(:,2);
 
+tic
 Ans1 = prod(holdTime(time, distance))
+toc
+tic
+for i = 1:length(time)
+    each(i) = calcWins(time(i), distance(i));
+end
+Ans1Alt = prod(each)
+toc
 
 %%
 
 time = double(strjoin(string(time),''));
 distance = double(strjoin(string(distance),''));
 
+tic
 Ans2 = holdTime(time, distance)
+toc
+tic
+Ans2Alt = calcWins(time, distance)
+toc
 
 %%
 function methods = holdTime(time, distance)
@@ -27,7 +40,13 @@ for i = 1:length(time)
         if attempt>record
             methods(i) = methods(i)+1;
         end
-        
     end
 end
 end
+
+function wins = calcWins(time, distance)
+discr = sqrt(time^2 - 4 * (distance + 1));
+x = [ceil((time - discr) / 2), floor((time + discr) / 2)];
+wins = x(2) - x(1) + 1;
+end
+
