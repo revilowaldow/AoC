f5481b8e4e8c2400443413b8db012f63ff461bc7
revilowaldow <oliver@warlow.engineer>
2023-12-09 13:33:43 +0000

    2023 09

---
+++ 2023/09/AoC_09.m
@@ -0,0 +1,34 @@
+clear
+opts = detectImportOptions("09_input.txt");
+input = readmatrix("09_input.txt",opts)
+
+%%
+predictions = zeros([length(input) 1]);
+
+for i = 1:length(input)
+    predictions(i) = n(input(i,:));
+end
+
+Ans1 = sum(predictions)
+
+%%
+
+for i = 1:length(input)
+    predictions(i) = n(flip(input(i,:)));
+end
+
+Ans2 = sum(predictions)
+
+%%
+
+function out = n(line)
+    if all(line==0)
+        out = 0;
+        return;
+    end
+    m = zeros(1, length(line)-1);
+    for i = 1:(length(line)-1)
+        m(i) = line(i+1) - line(i);
+    end
+    out = line(end) + n(m);
+end
