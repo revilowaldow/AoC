5fada0faba00cd6f071008a7951c651d77a86116
revilowaldow <oliver@warlow.engineer>
2023-12-14 13:50:25 +0000

    boilerplate

---
+++ makeFiles.m
@@ -0,0 +1,11 @@
+function makeFiles(y)
+mkdir(y)
+for idx = 1:25
+    mkdir(fullfile(y,sprintf("%02i",idx)))
+    fn = fullfile(y,sprintf("%02i",idx),"AoC_" + sprintf("%02i",idx)+".txt");
+    writematrix("clear",fn,QuoteStrings=false)
+    movefile(fn,fn.replace(".txt",".m"))
+    writelines("",fullfile(y,sprintf("%02i",idx),sprintf("%02i",idx)+"_inputDemo.txt"))
+    writelines("",fullfile(y,sprintf("%02i",idx),sprintf("%02i",idx)+"_input.txt"))
+end
+end
