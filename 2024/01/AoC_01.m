705c13b273e48c6cef99f01ffeb674d2d11410aa
revilowaldow <52298102+revilowaldow@users.noreply.github.com>
2024-12-10 20:44:09 +0000

    2024 01

--- 2024/01/AoC_01.m
+++ 2024/01/AoC_01.m
@@ -1,1 +1,16 @@
 clear
+opts = delimitedTextImportOptions("NumVariables", 2);
+%opts.DataLines = [1, Inf];
+opts.Delimiter = " ";
+opts.ConsecutiveDelimitersRule = "join";
+opts.VariableTypes = ["double", "double"];
+x01_input = readtable("01_input.txt", opts);
+
+Ans1 = sum(abs(sort(x01_input.Var1)-sort(x01_input.Var2)))
+
+similarity = 0;
+for i=1:height(x01_input)
+    similarity = similarity + (x01_input.Var1(i) * sum(x01_input.Var1(i)==x01_input.Var2));
+end
+
+Ans2 = similarity
